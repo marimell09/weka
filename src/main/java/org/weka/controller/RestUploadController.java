7db59fc693c2880d024df85e094976c048233586
MarianaCesar <mari_carvalhomello@hotmail.com>
2017-07-27 19:02:43 -0300

    Novo controler para upload de arquivo.

---
+++ src/main/java/org/weka/controller/RestUploadController.java
@@ -0,0 +1,73 @@
+package org.weka.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RestUploadController {
+	
+	private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
+
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "C://temp//";
+
+    // 3.1.1 Single file upload
+    @RequestMapping("/api/upload")
+    // If not @RestController, uncomment this
+    //@ResponseBody
+    public ResponseEntity<?> uploadFile(
+            @RequestParam("file") MultipartFile uploadfile) {
+
+        logger.debug("Single file upload!");
+
+        if (uploadfile.isEmpty()) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(uploadfile));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded - " +
+                uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+
+    }
+    
+  //save file
+    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+
+        for (MultipartFile file : files) {
+
+            if (file.isEmpty()) {
+                continue; //next pls
+            }
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+        }
+        
+    }
+
+}
--- src/main/java/org/weka/model/Arquivo.java
+++ src/main/java/org/weka/model/Arquivo.java
@@ -2,12 +2,9 @@ package org.weka.model;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import weka.core.Instances;
 
