1a9625cd0762b1f69a59bf4817ff00856a0172f5
MarianaCesar <mari_carvalhomello@hotmail.com>
2017-07-30 02:27:42 -0300

    Changes made based on the tutorial from Mineda.

--- src/main/java/org/weka/controller/RestUploadController.java
+++ src/main/java/org/weka/controller/RestUploadController.java
@@ -1,17 +1,5 @@
 package org.weka.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,19 +8,34 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.weka.model.UploadModel;
+@CrossOrigin(origins="*",maxAge=3600)
+@RestController
 public class RestUploadController {
 	
 	private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
 
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "C://temp//";
+    private static String UPLOADED_FOLDER = "C://temp1//";
 
     // 3.1.1 Single file upload
-    @RequestMapping("/api/upload")
+    @RequestMapping(value = "api/upload", method = RequestMethod.POST)
     // If not @RestController, uncomment this
     //@ResponseBody
-    public ResponseEntity<?> uploadFile(
-            @RequestParam("file") MultipartFile uploadfile) {
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile) {
 
         logger.debug("Single file upload!");
 
@@ -50,6 +53,53 @@ public class RestUploadController {
 
         return new ResponseEntity("Successfully uploaded - " +
                 uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+
+    }
+    
+    // 3.1.2 Multiple file upload
+    @RequestMapping("api/upload/multi")
+    public ResponseEntity<?> uploadFileMulti(
+            @RequestParam("extraField") String extraField,
+            @RequestParam("files") MultipartFile[] uploadfiles) {
+
+        logger.debug("Multiple file upload!");
+
+        // Get file name
+        String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
+                .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
+
+        if (StringUtils.isEmpty(uploadedFileName)) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(uploadfiles));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded - "
+                + uploadedFileName, HttpStatus.OK);
+
+    }
+
+    // 3.1.3 maps html form to a Model
+    @RequestMapping("api/upload/multi/model")
+    public ResponseEntity<?> multiUploadFileModel(@ModelAttribute UploadModel model) {
+
+        logger.debug("Multiple file upload! With UploadModel");
+
+        try {
+
+            saveUploadedFiles(Arrays.asList(model.getFiles()));
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded!", HttpStatus.OK);
 
     }
     
